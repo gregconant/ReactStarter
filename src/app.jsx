fa845d7491871fedfef8103390ad6300ae645a30
Greg Conant <greg.conant@gmail.com>
2016-06-15 07:03:14 -0400

    gc - setting firebase required version to 2.4.2 from default of 3.x (the latest at the time) to match the course videos.

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,29 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var rootUrl = 'https://learningreact-3fa38.firebaseio.com';
+
+var myFirebaseRef = new Firebase(rootUrl + 'items/');
+
+myFirebaseRef.set({
+  title: "Hello React Data!",
+  author: "Firebase",
+  location: {
+    city: "Philadelphia",
+    state: "PA",
+    zip: 19103
+  }
+});
 
 var Hello = React.createClass({
+  mixins: [ ReactFire ], // copies methods from mixins to this component
+  componentWillMount: function() {
+    this.bindAsObject(myFirebaseRef, 'items');
+    // this.state.items = {} from Firebase
+  },
   render: function() {
+    console.log(this.state);
     return <h1 className="red">
       Hello!
     </h1>
