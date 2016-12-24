b0621e7f3a6bd6b4e81d6517922bd20be6e88c80
Greg Conant <greg.conant@gmail.com>
2016-12-24 08:31:41 -0500

    gc - section 5, lecture 32 - hiding list until data from firebase is loaded; then, fading in.

--- src/app.jsx
+++ src/app.jsx
@@ -10,28 +10,37 @@ var rootUrl = 'https://learningreact-3fa38.firebaseio.com/';
 var myFirebaseRef = new Firebase(rootUrl + 'items/');
 
 var App = React.createClass({
-  mixins: [ ReactFire ], // copies methods from mixins to this component
-  getInitialState: function() {
-    return {
-      items: {}
-    }
-  },
-  componentWillMount: function() {
-    this.bindAsObject(myFirebaseRef, 'items'); // binds to 'this.state.items'
-    // this.state.items = {} from Firebase
-  },
-  render: function() {
-    return <div className="row panel panel-default">
-      <div className="col-md-8 col-md-offset-2">
-        <h2 className="text-center">
-          To-Do List
-        </h2>
-        <Header itemsStore={this.firebaseRefs.items} />
-        <List items={this.state.items} />
-      </div>
-    </div>
+    mixins: [ReactFire], // copies methods from mixins to this component
+    getInitialState: function () {
+        return {
+            items: {},
+            loaded: false
+        }
+    },
+    componentWillMount: function () {
 
-  }
+        this.bindAsObject(myFirebaseRef, 'items'); // binds to 'this.state.items'
+        // this.state.items = {} from Firebase
+        myFirebaseRef.on('value', this.handleDataLoaded);
+    },
+    render: function () {
+        return <div className="row panel panel-default">
+            <div className="col-md-8 col-md-offset-2">
+                <h2 className="text-center">
+                    To-Do List
+                </h2>
+                <Header itemsStore={this.firebaseRefs.items}/>
+                <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
+
+                    <List items={this.state.items}/>
+                </div>
+            </div>
+        </div>
+
+    },
+    handleDataLoaded: function () {
+        this.setState({loaded: true});
+    }
 });
 
 var element = React.createElement(App, {});
